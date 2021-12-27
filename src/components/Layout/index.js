7bb64424ae3b8f78924ab8a867faf6e00782fa9d
Melanie Cabrera <melanie16203@gmail.com>
2021-12-27 10:13:19 -0300

    Netlify CMS integrated

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -5,6 +5,7 @@ if (typeof window !== 'undefined') AOS = require('aos')
 import 'aos/dist/aos.css'
 
 import Header from 'components/Header'
+import Footer from 'components/Footer'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
